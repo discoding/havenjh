b6519113f5d07ee1ce88c380f03d751e15213f42
discoding <80160415+discoding@users.noreply.github.com>
2026-08-17 15:33:19 +0200

    Add BuienradarWidget for displaying weather radar

---
+++ lib/widgets/buienradar_widget.dart
@@ -0,0 +1,66 @@
+import 'dart:ui_web' as ui_web;
+import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
+
+class BuienradarWidget extends StatefulWidget {
+  const BuienradarWidget({super.key});
+
+  @override
+  State<BuienradarWidget> createState() => _BuienradarWidgetState();
+}
+
+class _BuienradarWidgetState extends State<BuienradarWidget> {
+  static const String _viewId = 'buienradar-radar';
+
+  @override
+  void initState() {
+    super.initState();
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      _viewId,
+      (int viewId) {
+        final iframe = web.HTMLIFrameElement()
+          ..src =
+              'https://image.buienradar.nl/2.0/image/single/RadarMapRainNL'
+              '?height=512'
+              '&width=500'
+              '&renderBackground=True'
+              '&renderBranding=True'
+              '&renderText=True'
+          ..style.border = '0'
+          ..style.width = '100%'
+          ..style.height = '100%';
+
+        return iframe;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(12),
+            child: Text(
+              'Buienradar',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 512,
+            width: double.infinity,
+            child: HtmlElementView(
+              viewType: _viewId,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
